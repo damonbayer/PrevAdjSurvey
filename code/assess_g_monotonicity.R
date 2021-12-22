a8ec8b8c2465df762bf84928645059aa352b8d01
Damon Bayer <damon@damonbayer.com>
2021-12-22 12:45:12 -0600

    Revised monotonicity

--- code/assess_g_monotonicity.R
+++ code/assess_g_monotonicity.R
@@ -4,7 +4,7 @@ g <- function(theta_1, phi_n, phi_p) {
   if ((phi_n < phi_p) & (phi_p < theta_1)) {
     return(1)
   } else if ((phi_p >= theta_1) & (theta_1 >= phi_n)) {
-    return((theta_1 - phi_n) / (phi_p - phi_n))  
+    return((theta_1 - phi_n) / (phi_p - phi_n))
   } else {
     return(0)
   }
@@ -12,32 +12,69 @@ g <- function(theta_1, phi_n, phi_p) {
 
 monotonic <- function(x) all(x == cummax(x)) | all(x == cummin(x))
 
-tmp <- 
+tmp <-
   crossing(theta_1 = seq(0, 1, 0.005),
-         phi_n = seq(0, 1, 0.01),
-         phi_p = seq(0, 1, 0.01)) %>% 
-  filter(phi_p > phi_n) %>% 
-  filter(phi_p >= theta_1 & theta_1 >= phi_n) %>% 
+           phi_n = seq(0, 1, 0.01),
+           phi_p = seq(0, 1, 0.01)) %>%
+  filter(phi_p > phi_n) %>%
+  filter(phi_p >= theta_1 & theta_1 >= phi_n) %>%
   mutate(g = pmap_dbl(list(theta_1 = theta_1, phi_n = phi_n, phi_p = phi_p),
-                           function(theta_1, phi_n, phi_p) g(theta_1, phi_n, phi_p)))
+                      function(theta_1, phi_n, phi_p) g(theta_1, phi_n, phi_p)))
 
-tmp %>% 
-  group_by(theta_1, phi_n) %>% 
-  arrange(phi_p) %>% 
-  summarize(phi_p = list(phi_p), .groups = "drop") %>% 
-  mutate(monotonic = map_lgl(phi_p, monotonic)) %>% 
+tmp %>%
+  group_by(theta_1, phi_n) %>%
+  arrange(phi_p) %>%
+  summarize(phi_p = list(phi_p), .groups = "drop") %>%
+  mutate(monotonic = map_lgl(phi_p, monotonic)) %>%
   summarize(mean(monotonic))
 
-tmp %>% 
-  group_by(phi_p, phi_n) %>% 
-  arrange(theta_1) %>% 
-  summarize(theta_1 = list(theta_1), .groups = "drop") %>% 
-  mutate(monotonic = map_lgl(theta_1, monotonic)) %>% 
+tmp %>%
+  group_by(phi_p, phi_n) %>%
+  arrange(theta_1) %>%
+  summarize(theta_1 = list(theta_1), .groups = "drop") %>%
+  mutate(monotonic = map_lgl(theta_1, monotonic)) %>%
   summarize(mean(monotonic))
 
-tmp %>% 
-  group_by(theta_1, phi_p) %>% 
-  arrange(phi_n) %>% 
-  summarize(phi_n = list(phi_n), .groups = "drop") %>% 
-  mutate(monotonic = map_lgl(phi_n, monotonic)) %>% 
+tmp %>%
+  group_by(theta_1, phi_p) %>%
+  arrange(phi_n) %>%
+  summarize(phi_n = list(phi_n), .groups = "drop") %>%
+  mutate(monotonic = map_lgl(phi_n, monotonic)) %>%
   summarize(mean(monotonic))
+
+
+
+
+# -------------------------------------------------------------------------
+
+
+tmp <-
+  crossing(theta_1 = seq(0, 1, 0.005),
+           phi_n = seq(0, 1, 0.01),
+           phi_p = seq(0, 1, 0.01)) %>%
+  mutate(g = pmap_dbl(list(theta_1 = theta_1, phi_n = phi_n, phi_p = phi_p),
+                      function(theta_1, phi_n, phi_p) g(theta_1, phi_n, phi_p))) %>%
+  replace_na(list(g = 0))
+
+tmp %>%
+  filter(phi_n == theta_1) %>%
+  group_by(phi_n, theta_1) %>%
+  arrange(phi_p) %>%
+  summarize(monotonic(g), .groups = "drop") %>%
+  summarize(mean(`monotonic(g)`))
+
+tmp %>%
+  filter(phi_n == phi_p) %>%
+  group_by(phi_n, phi_p) %>%
+  arrange(theta_1) %>%
+  summarize(monotonic(g), .groups = "drop") %>%
+  summarize(mean(`monotonic(g)`))
+
+
+tmp %>%
+  filter(theta_1 == phi_p) %>%
+  group_by(theta_1, phi_p) %>%
+  arrange(phi_n) %>%
+  summarize(monotonic(g), .groups = "drop") %>%
+  summarize(mean(`monotonic(g)`))
+
